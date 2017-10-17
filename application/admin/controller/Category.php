50351b217df53bc420106541218c9743295e5da3
stubbornfan <stubbornfan@foxmail.com>
2017-10-17 17:36:04 +0800

    5-2验证机制

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -1,20 +1,34 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+
 class Category extends Controller
 {
     public function index()
     {
         return $this->fetch();
     }
-    public function add()
-    {
+    public function add(){
         return $this->fetch();
     }
     
     public function save()
     {
-        return $this->fetch();
+        $data = input('post.');
+        $validate = validate('Category');
+        if( !$validate->scene('add')->check($data)){
+            $this->error($validate->getError());
+        }
+        
+        //提交数据到model层
+        $res = model('Category')->add($data);
+        if($res){
+            $this->success('新增成功');
+        } else {
+            $this->error('新增失败');
+        }
+        
+        
     }
 
 }
--- application/common/model/Category.php
+++ application/common/model/Category.php
@@ -1,9 +1,14 @@
 <?php
 namespace app\common\model;
-
 use think\Model;
 
 class Category extends Model
 {
+    protected $autoWriteTimestamp = true;
+    
+    public function add($data){
+        $data['status']= 1;
+       return $this->save($data);
+    }
 
 }
